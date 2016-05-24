5d74962db370d78ebfd6a8ff9efc23e388439b59
Aquatic Pond <etheriad@gmail.com>
2016-05-24 11:04:57 -0400

    Move this; Naming consistency;

--- src/Aquatic/FedEx/Request/Shipment/Track.php
+++ src/Aquatic/FedEx/Request/Shipment/Track.php
@@ -1,27 +1,23 @@
 <?php
 
-namespace Aquatic\FedEx\Shipment;
+namespace Aquatic\FedEx\Request\Shipment;
 
 use Aquatic\FedEx\Request;
-use Exception;
 
 class Track extends Request
 {
-    public $_version = '10';
+    public $_version = '10.0.0';
     public $_serviceId = 'trck';
     public $_wsdl = 'TrackService_v10.wsdl';
     public $_soapMethod = 'track';
 
-    public function __construct(int $tracking_code)
+    public function __construct(int $tracking_number)
     {
-        if(!$tracking_code)
-            throw new Exception("You must pass a tracking code when creating a new Shipment Tracking request.");
-
         $this->data = [
             'SelectionDetails' => [
                 'PackageIdentifier' => [
                     'Type' => 'TRACKING_NUMBER_OR_DOORTAG',
-                    'Value' => $tracking_code
+                    'Value' => $tracking_number
                 ]
             ],
             'ProcessingOptions' => 'INCLUDE_DETAILED_SCANS'
