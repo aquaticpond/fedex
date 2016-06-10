3969f956fd25d059e9e3a9cff004b9667b8c8376
Aquatic Pond <etheriad@gmail.com>
2016-06-10 13:36:13 -0400

    Yer a facade harry!

---
+++ src/Aquatic/FedEx.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Aquatic;
+
+use Aquatic\FedEx\Contract\Address;
+use Aquatic\FedEx\Contract\Shipment;
+use Aquatic\FedEx\Response\Contract as ResponseContract;
+use Aquatic\FedEx\Request\Shipment\Track as TrackShipmentRequest;
+use Aquatic\FedEx\Response\Shipment\Track as TrackParser;
+use Aquatic\FedEx\Request\Shipment\CustomsAndDuties as CustomsAndDutiesRequest;
+use Aquatic\FedEx\Response\Shipment\CustomsAndDuties as CustomsAndDutiesParser;
+
+// Facade for FedEx requests
+class FedEx
+{
+    public static function trackShipment(int $tracking_number): ResponseContract
+    {
+        return (new TrackShipmentRequest($tracking_number))
+            ->setCredentials(getenv('FEDEX_KEY'), getenv('FEDEX_PASSWORD'), getenv('FEDEX_ACCOUNT_NUMBER'), getenv('FEDEX_METER_NUMBER'))
+            ->send(new TrackParser);
+    }
+    
+    public static function customsAndDuties(Shipment $shipment, Address $shipper)
+    {
+        return (new CustomsAndDutiesRequest($shipment, $shipper))
+            ->setCredentials(getenv('FEDEX_KEY'), getenv('FEDEX_PASSWORD'), getenv('FEDEX_ACCOUNT_NUMBER'), getenv('FEDEX_METER_NUMBER'))
+            ->send();
+    }
+}
