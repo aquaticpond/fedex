8b37190ed017f8776186f4f05b28fcf1905bee66
Aquatic Pond <etheriad@gmail.com>
2016-06-13 14:33:58 -0400

    Could be this, could be that.

--- src/Aquatic/FedEx/Response/Mixin/NotificationParser.php
+++ src/Aquatic/FedEx/Response/Mixin/NotificationParser.php
@@ -11,6 +11,8 @@ trait NotificationParser
         $this->notifications_highest_severity = $response->HighestSeverity;
         $this->notifications = [];
 
+        $notifications = is_array($response->Notifications) ? $response->Notifications : [$response->Notifications];
+
         foreach($response->Notifications as $note)
         {
             $notification = new Notification($note->Severity, $note->Source, $note->Code, $note->Message, $note->LocalizedMessage);
