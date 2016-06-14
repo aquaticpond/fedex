ba8148a245c9d8219d9969676a657bc40b8bd38a
Aquatic Pond <etheriad@gmail.com>
2016-06-14 10:49:54 -0400

    Add Validate Address Request

---
+++ src/Aquatic/FedEx/Request/ValidateAddress.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Aquatic\FedEx\Request;
+
+use Aquatic\FedEx\Request;
+use Aquatic\FedEx\Contract\Address;
+
+class ValidateAddress extends Request
+{
+    public $_version = '4.0.0';
+    public $_serviceId = 'aval';
+    public $_wsdl = 'AddressValidationService_v4.wsdl';
+    public $_soapMethod = 'addressValidation';
+
+
+    public function __construct(Address $address)
+    {
+        $this->data = [
+            'AddressToValidate' => [
+                'Address' => [
+                    'StreetLines' => $address->getStreet(),
+                    'City'        => $address->getCity(),
+                    'StateOrProvinceCode' => $address->getStateCode(),
+                    'PostalCode'  => $address->getPostCode(),
+                    'CountryCode' => $address->getCountryCode(),
+                ]
+            ]
+        ];
+    }
+}
