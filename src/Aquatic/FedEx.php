2a9051643e5eacf80dcaaddf9ddc185b25eb06f0
Aquatic Pond <etheriad@gmail.com>
2016-06-15 12:18:18 -0400

    Parser for address validation

--- src/Aquatic/FedEx.php
+++ src/Aquatic/FedEx.php
@@ -6,10 +6,11 @@ use Aquatic\FedEx\Contract\Address;
 use Aquatic\FedEx\Contract\Shipment;
 use Aquatic\FedEx\Response\Contract as ResponseContract;
 use Aquatic\FedEx\Request\ValidateAddress as ValidateAddressRequest;
+use Aquatic\FedEx\Response\ValidateAddress as ValidateAddressResponse;
 use Aquatic\FedEx\Request\Shipment\Track as TrackShipmentRequest;
-use Aquatic\FedEx\Response\Shipment\Track as TrackParser;
+use Aquatic\FedEx\Response\Shipment\Track as TrackShipmentResponse;
 use Aquatic\FedEx\Request\Shipment\CustomsAndDuties as CustomsAndDutiesRequest;
-use Aquatic\FedEx\Response\Shipment\CustomsAndDuties as CustomsAndDutiesParser;
+use Aquatic\FedEx\Response\Shipment\CustomsAndDuties as CustomsAndDutiesResponse;
 
 // Facade for FedEx requests
 class FedEx
@@ -18,20 +19,20 @@ class FedEx
     {
         return (new TrackShipmentRequest($tracking_number))
             ->setCredentials(getenv('FEDEX_KEY'), getenv('FEDEX_PASSWORD'), getenv('FEDEX_ACCOUNT_NUMBER'), getenv('FEDEX_METER_NUMBER'))
-            ->send(new TrackParser);
+            ->send(new TrackShipmentResponse);
     }
     
     public static function customsAndDuties(Shipment $shipment, Address $shipper)
     {
         return (new CustomsAndDutiesRequest($shipment, $shipper))
             ->setCredentials(getenv('FEDEX_KEY'), getenv('FEDEX_PASSWORD'), getenv('FEDEX_ACCOUNT_NUMBER'), getenv('FEDEX_METER_NUMBER'))
-            ->send(new CustomsAndDutiesParser($shipment->getItems()));
+            ->send(new CustomsAndDutiesResponse($shipment->getItems()));
     }
 
     public static function validateAddress(Address $address)
     {
         return (new ValidateAddressRequest($address))
             ->setCredentials(getenv('FEDEX_KEY'), getenv('FEDEX_PASSWORD'), getenv('FEDEX_ACCOUNT_NUMBER'), getenv('FEDEX_METER_NUMBER'))
-            ->send();
+            ->send(new ValidateAddressResponse);
     }
 }
